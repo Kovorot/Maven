ee4a9a7a2cb4490d7b7f4aad3f4b9acd01000872
Andrey <kovorot0875@mail.ru>
2023-05-05 07:53:11 +0300

    Maven added

---
+++ db/src/main/java/ru/netulogy/DbSettings.java
@@ -0,0 +1,12 @@
+package ru.netulogy;
+
+public class DbSettings {
+
+    private String name;
+    private String password;
+
+    public DbSettings(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
